4ae6c118e4806c67f03129db6a85f5630b965314
steven <alvaradosteven315@gmail.com>
2022-11-10 23:52:07 -0500

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      darkcyan: 'hsl(158, 36%, 37%)',
+      cream: 'hsl(30, 38%, 92%)',
+      darkblue: 'hsl(212, 21%, 14%)',
+      grayishblue: 'hsl(228, 12%, 48%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
+  },
+  plugins: [],
+}
