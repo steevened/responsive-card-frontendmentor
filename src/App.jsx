17bcd55ddf1899f8e366e2855d50d8e301f72484
steven <alvaradosteven315@gmail.com>
2022-11-11 11:10:33 -0500

    ending

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,24 @@ function App() {
     <div className="App h-[667px] lg:h-screen bg-[url(./design/desktop-design.jpg)] bg-no-repeat lg:flex lg:items-center lg:justify-center bg-cream  px-4 py-7 bg-[hsl(30, 38%, 92%)]">
       <div className="overflow-hidden lg:flex rounded-[10px] lg:w-[600px] lg:h-[450px] lg:mt-[40px]">
         <div className="basis-1/2 bg-[url(../images/image-product-desktop.jpg)] bg-cover bg-left"></div>
-        <div className="px-6 py-5 lg:px-8 bg-white lg:basis-1/2 ">
-          <h3 className="text-[15px] tracking-[2.5px] montse text-grayishblue">
+        <div className="px-6 py-5 lg:px-8 lg:py-7 bg-white lg:basis-1/2 ">
+          <h3 className="text-[15px] lg:mt-2 tracking-[2.5px] montse text-grayishblue">
             PERFUME
           </h3>
           <h1 className="text-[32px] font-bold leading-8 mt-2 fran">
             Gabrielle Essence Eau De Parfum
           </h1>
-          <p className="text-sm text-grayishblue leading-[23.5px] mt-4 montse">
+          <p className="text-sm text-grayishblue leading-[23.5px] mt-4 montse lg:mt-6">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <div className="flex mt-[14px] items-center gap-5">
+          <div className="flex mt-[14px] items-center gap-5 lg:mt-[17px]">
             <h2 className="text-[34px] text-darkcyan font-bold tracking-[0.1px]  fran">
               $149.99{" "}
             </h2>
             <h3 className="text-[13px] text-grayishblue  montse">$169.99</h3>
           </div>
-          <button className="rounded-lg mt-2 flex gap-[12px] h-[50px] w-full justify-center items-center bg-darkcyan hover:bg-darkblue">
+          <button className="rounded-lg mt-2 lg:m-5 flex gap-[12px] h-[50px] w-full justify-center items-center bg-darkcyan hover:bg-darkblue">
             <Cart />
             <h4 className="text-white text-[15px] font-bold"> Add to Cart</h4>
           </button>
